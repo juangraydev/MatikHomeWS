528df1285acad6ae2c71a78abfffedcdf89650ad
juangraydev <jwsabala@gmail.com>
2023-07-28 11:04:45 +0800

    t

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ var connection = mysql.createConnection({
     host: "containers-us-west-51.railway.app",
     user: "root",
     password: "PFriq1CEYtDFbwnj4wqm",
-    database: "railway"
+    database: "railway",
+    port: 7336
 });
 connection.connect(function(error){
 	if(!!error) {
