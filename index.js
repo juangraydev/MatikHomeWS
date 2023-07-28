432aef9d44565041661a7d50cf1753a5410c908d
juangraydev <jwsabala@gmail.com>
2023-07-28 10:19:46 +0800

    bard

--- index.js
+++ index.js
@@ -1,19 +1,143 @@
-const express = require('express');
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const cors = require('cors');
+// const httpServer = http.createServer(app);
+// const { Server } = require("socket.io");
+// var mysql = require('mysql');
+// var dbConn = require('./db')
+
+// app.use(
+//   cors({
+//     origin: "https://matikhomews-production.up.railway.app"
+//   })
+// )
+
+// const io = new Server(httpServer);
+// function get_device(homeId){
+//   let resp_data = []
+//   return new Promise(function(resolve, reject){
+//     let sql = `SELECT * FROM devices WHERE home_id = '${homeId.replaceAll("-","")}'`
+//     dbConn.query(sql, async function(err,rows)     {
+//       if(err) {
+//         reject(err)
+//       } else {
+//         var res_device = []
+//         for (let index = 0; index < rows.length; index++) {
+//           const element = rows[index];
+//           var device_channel = []
+//           await get_channel(element.id)
+//             .then((channel)=>{
+//               channel?.map((channel) => {
+//                 device_channel.push(channel)
+//               })
+//             })
+//             .catch((err)=>{
+//                 throw err
+//             })
+          
+//           res_device = res_device.concat(device_channel)
+//         }
+//         resolve(res_device);
+//       }
+//     });
+//   })
+// }
+
+// function get_channel(deviceId){
+//   return new Promise(function(resolve, reject){
+//     let sql = `SELECT * FROM channels WHERE device_id = '${deviceId}'`
+//     dbConn.query(sql,function(err,rows)     {
+//       if(err) {
+//         reject(err)
+//       } else {
+//         resolve(rows)
+//       }
+//     });
+//   })
+// }
+
+// function update_channel(channelData){
+//   return new Promise(function(resolve, reject){
+//     let sql = `UPDATE channels SET status = '{"on": ${channelData.status}}' WHERE id = "${channelData.channelId}";`
+//     console.log("update_channel", sql);
+//     dbConn.query(sql,function(err,rows)     {
+//       if(err) {
+//         reject(err)
+//       } else {
+//         resolve(rows)
+//       }
+//     });
+//   })
+// }
+
+// io.on("connection", (socket) => {
+//   let devices = []
+//   console.log('a user connected',socket.id);
+//   socket.on("home_devices", async(homeId) => { 
+//     console.log("recieve");
+//     await get_device(homeId.replaceAll("-",""))
+//       .then((res)=>{
+//         devices = res
+//       })
+//       .catch((err)=>{
+//           throw err
+//       })
+//     socket.emit("home_devices", devices)
+//   });
+
+//   socket.on("channel", async(homeId, data) => { 
+//     console.log("recieve", homeId, data);
+//     await update_channel(JSON.parse(data))
+//       .then(async (res) => {
+//         console.log("res", res);
+//         await get_device(homeId.replaceAll("-",""))
+//           .then((res)=>{
+//             devices = res
+//           })
+//           .catch((err)=>{
+//               throw err
+//           })
+//         socket.emit("home_devices", devices)
+//       })
+//     // await get_device(homeId.replaceAll("-",""))
+//     //   .then((res)=>{
+//     //     devices = res
+//     //   })
+//     //   .catch((err)=>{
+//     //       throw err
+//     //   })
+//     // socket.emit("home_devices", devices)
+//   });
+// });
+
+
+httpServer.listen(8001, () => {
+  console.log('listening on *:8001');
+});
+
+
+
+const express = require("express");
+const socketio = require("socket.io");
+
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const httpServer = http.createServer(app);
-const { Server } = require("socket.io");
-var mysql = require('mysql');
-var dbConn = require('./db')
 
-app.use(
-  cors({
-    origin: "https://matikhomews-production.up.railway.app"
-  })
-)
+app.use(express.static("public"));
 
-const io = new Server(httpServer);
+const io = socketio(app);
+
+// server.on("connection", (socket) => {
+//   console.log("New connection from", socket.id);
+
+//   socket.on("message", (data) => {
+//     console.log("Received message from", socket.id, data);
+
+//     socket.broadcast("message", data);
+//   });
+// });
+
+
 function get_device(homeId){
   let resp_data = []
   return new Promise(function(resolve, reject){
@@ -111,7 +235,12 @@ io.on("connection", (socket) => {
   });
 });
 
-
-httpServer.listen(8001, () => {
-  console.log('listening on *:8001');
+server.options({
+  cors: {
+    origin: "*",
+  },
 });
+
+server.listen(8080);
+
+console.log("Server listening on port 8080");
