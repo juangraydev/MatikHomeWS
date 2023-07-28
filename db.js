a65608988535ff27985d0c003c56c33b184b0ee9
juangraydev <jwsabala@gmail.com>
2023-07-28 13:45:51 +0800

    latest

--- db.js
+++ db.js
@@ -2,7 +2,11 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test'
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
+
+module.exports = connection
