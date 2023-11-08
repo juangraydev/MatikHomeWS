560ad0974d3d6e1888c06c12163d5841a4aec817
juangraydev <jwsabala@gmail.com>
2023-11-09 02:14:51 +0800

    please

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    host: "containers-us-west-111.railway.app",
+    user: "root",
+    password: "3zjhXq8tOTpXG2XwGdH0",
+    database: "railway",
     port: process.env.MYSQLPORT
 });
 
